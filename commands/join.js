53a65cd1c5aa221afc2bd99e6efa5ec85c6d4232
Carson-Key <keyc@oregonstate.edu>
2022-05-04 13:37:58 -0700

    Add speech recognition

--- commands/join.js
+++ commands/join.js
@@ -2,8 +2,11 @@ const { entersState, joinVoiceChannel, EndBehaviorType, VoiceConnection, VoiceCo
 const fs = require("fs");
 const { pipeline } = require('node:stream');
 const prism = require('prism-media');
+const child = require('child_process');
 
-function createListeningStream(receiver, userId) {
+const pythonPath = __dirname + "/../bin/speech_to_text.py"
+
+function createListeningStream(receiver, userId, textChannel, client) {
     const path = __dirname + "/../recordings/" + Date.now() + "-" + userId + ".ogg";
     
     const opusStream = receiver.subscribe(userId, {
@@ -28,12 +31,23 @@ function createListeningStream(receiver, userId) {
             console.warn("Error recording file " + path + ": " + err.message);
         } else {
             console.log("Recorded " + path);
+            let py = child.spawn('python3', [
+                pythonPath,
+                path
+            ]);
+            py.stdout.on('data', (data) => {
+                client.channels.cache
+                    .get(textChannel.channelId)
+                    .send("<@" + userId + ">" + " " + data.toString());
+            });
         }
     });
 }
 
 const join = async (client, message) => {
     const voiceChannel = message.member.voice.channel;
+    const textChannel = message;
+
     if (!voiceChannel) {
         console.log("You are not in a voice channel")
         return message.channel.send("Please join a voice channel first!")
@@ -52,7 +66,7 @@ const join = async (client, message) => {
     
             receiver.speaking.on('start', (userId) => {
                 console.log(message.member.user.username + " Started Speaking")
-                createListeningStream(receiver, userId)
+                createListeningStream(receiver, userId, textChannel, client)
             });
         } catch (error) {
             console.log(error);
